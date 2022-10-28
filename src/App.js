2b01e0430128c7553ca471750caaefeb0c9f7f7d
Jenna Del Vigna <jennadelvigna@gmail.com>
2022-10-28 16:49:15 -0400

    added about content and created side nav

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css'
-import albums from './data.json'
+// import albums from './data.json'
 
 const App = () => {
   return (
     <div className="App">
-      <header>
+      <nav class="nav">
+        <p>Home</p>
+        <p>PULP</p>
+        <p>Solo Albums</p>
+        <p>Side Projects</p>
+        <p>Mixtape</p>
+      </nav>
+      <header class="header">
         <h1>Jarvis Cocker</h1>
+        <p>
+          Born 19 September 1963, Jarv... is an English musician,radio
+          presenter, author, dj, and sex god. As the founder, frontman, lyricist
+          and only consistent member of the band Pulp, he became a figurehead of
+          the Britpop genre of the mid-1990s. Following Pulp's hiatus, Cocker
+          pursued a solo career and is the king of side projects.
+        </p>
       </header>
-      <section>
+      <section class="pulp">
         <h2>PULP</h2>
       </section>
-      <section>
+      <section class="solo">
         <h2>Solo Albums</h2>
       </section>
-      <section>
+      <section class="side">
         <h2>Side Projects</h2>
       </section>
+      <footer>
+        <p>Copyright JRDV 2023</p>
+        <p>jennadelvigna@gmail.com</p>
+      </footer>
     </div>
   )
 }
