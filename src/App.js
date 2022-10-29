915d00381a8caed3ba40027244072ca93b16537f
Jenna Del Vigna <jennadelvigna@gmail.com>
2022-10-28 22:20:32 -0400

    added image from dropbox, fixed margin to clear sticky nav

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ const App = () => {
       </nav>
       <header class="header">
         <h1>Jarvis Cocker</h1>
+        <img
+          id="unionJack"
+          src="https://previews.dropbox.com/p/thumb/ABtRQAZFE5jKuERVqAt8HOvFw5glPSxJ3-WowA53R0g_90nRlTp9g8MJPsfs7L3MqIDGMEFfMEqvx1p5mqoVUEM812OrIX2FW3oFypdf_sdBDpPijSBwc8nruc2liSXBM6SzjyOOlIv0uisFg_DzCXhU9egPjUFCLqHygeeB6jqsBiDr4AOupGVQDqYmDAuQ2cbJpwKFbGFBZyuEnUEUo6DaqRiIr75z170qhYrN14cRaEu1X9C7-LW0uBR0_a5GZlVofBG2rB8O9Do0ojdIVkybvkh1z8tjDec-cMGyBXY4tSlGYpMk60HprIBZlEqh0YAKu623qqSZgcfMtUXJqM5tQ0CPrw4hxRSEBIwFrTiQ3o0ootI4BXim6LULo6E5SY4/p.png"
+          alt="Illustration of Jarvis Cocker"
+        />
         <p>
           Born 19 September 1963, Jarv... is an English musician,radio
           presenter, author, dj, and sex god. As the founder, frontman, lyricist
