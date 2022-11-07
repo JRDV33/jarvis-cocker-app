576f0fc3cb957fc2a70ee7e8cad846d6b86a739e
Jenna Del Vigna <jennadelvigna@gmail.com>
2022-11-07 15:52:51 -0500

    added illustration and fixed some CSS for drop shadows

--- src/App.js
+++ src/App.js
@@ -20,6 +20,11 @@ const App = () => {
   return (
     <div className="App">
       <header className="header">
+        <img
+          id="unionJack"
+          src="https://previews.dropbox.com/p/thumb/ABt-Ognz1C8yJO7xau8poq_Y6K1W0osy-JCfPyTQm3gaULQ8rMrgQgRzeLYMJJelrus-OavmUuldquFvGJseTCtfds0y9T1nG9-XPQr7nYLBkLsJme2OvMRWf59RDALaaQ94WyZdsu1IAlvIGpAKGyqeniHa9YOUdb-giGt44sBx5cH7Pqvcfwvd0mVapHbMM47YAcdBmgfrtrucfo27ZmSB4CrXKkl2G-Lo15pcUaIVvah2BEABY4qCC1bkXFR0nDYyOMJ3O1Gnzto4FZS2UbDn03AFAMIzcUiYQszTOkIEI98IDanLui3yiuA7I5q-K5BVLC_KHO4m5cao6qAEhiJsf4R1EEyHH2fJH3K1eHRid2SI3ncInNhJve78jfyq3CM/p.png"
+          alt="Illustration of Jarvis Cocker"
+        />
         <h1>Jarvis Cocker</h1>
         <p>
           Born 19 September 1963, Jarv... is an English musician,radio
@@ -32,11 +37,6 @@ const App = () => {
           <button onClick={getQuote}>New Quote</button>
           <Quote quote={quote} />
         </div>
-        {/* <img
-          id="unionJack"
-          src="https://previews.dropbox.com/p/thumb/ABtRQAZFE5jKuERVqAt8HOvFw5glPSxJ3-WowA53R0g_90nRlTp9g8MJPsfs7L3MqIDGMEFfMEqvx1p5mqoVUEM812OrIX2FW3oFypdf_sdBDpPijSBwc8nruc2liSXBM6SzjyOOlIv0uisFg_DzCXhU9egPjUFCLqHygeeB6jqsBiDr4AOupGVQDqYmDAuQ2cbJpwKFbGFBZyuEnUEUo6DaqRiIr75z170qhYrN14cRaEu1X9C7-LW0uBR0_a5GZlVofBG2rB8O9Do0ojdIVkybvkh1z8tjDec-cMGyBXY4tSlGYpMk60HprIBZlEqh0YAKu623qqSZgcfMtUXJqM5tQ0CPrw4hxRSEBIwFrTiQ3o0ootI4BXim6LULo6E5SY4/p.png"
-          alt="Illustration of Jarvis Cocker"
-        /> */}
       </header>
       <section className="pulp">
         <h2>PULP Music</h2>
@@ -70,6 +70,10 @@ const App = () => {
       </section>
       <aside className="mixtape">
         <h3>Mixtape</h3>
+        {/* <img
+          id="thinking"
+          src="https://previews.dropbox.com/p/thumb/ABtthaQUKnzMCr5OKUH40pnZnaSnbBzF-NP42b3pwKKA8v2Nqo40V3q4FIQzz1gsmLYIop9Lb4PLqbtxIhTv14eNZvpczeIp2w_BAUA6K4ZsadMuJWGAxdJLq978LLVaSOx2zJ3yBEem-5tUbyj7qaTuVtXyrOvbu3zD69W3BZDqfRmtrbLVPyIG3sRDts-Q9FmcOEO6Gm8n3iLLIpKqGDGIY_b0w3p0jdNL0McR44gDGvFHzkGXs7eTtze5xNFWlfj5Gy4r76nCsjyigif_JdrotK6ihR6xNYve7ZHU1rPfDvBeqaqXDjVLPqOjqdwwcbkdF4_ivOeodmZogwrVi58mvqBmtpD05tskT2z8iTzhw5UJsPr7lZzBANr-sVLEDC8/p.png"
+        /> */}
         <p>This is a collection of favorite songs compiled in one list.</p>
         <div>
           {albumArray.map((video) => (
